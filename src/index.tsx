5909a94580f18b125238d97ee6ae26b9558f847b
antaresofficial <antaresofficial@icloud.com>
2022-01-31 00:22:48 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import "./index.css";
 
+console.log(
+  "I will love you till the end of time, \nI would wait a million years. \nPromise you'll remember that you're mine."
+);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
