ef79f352aebba76a359f71678b4ef1b20c9f8f72
antaresofficial <antaresofficial@icloud.com>
2022-01-31 12:28:24 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { AboutEn } from "../constants/en/about";
 import { AboutRu } from "../constants/ru/about";
 
@@ -8,24 +9,39 @@ const About: React.FC<{ lang: boolean; color: string }> = ({ lang, color }) => {
       <h1>{name}</h1>
       <hr />
 
-      {content.map((e) => {
-        const word = e.match(/\[(.+?)\]/);
-        const text = e.replace(
-          /\[(.+?)\]/,
-          ` <style type="text/css">
+      <Content color={color}>
+        {content.map((e) => {
+          const word = e.match(/\[(.+?)\]/);
+          const text = e.replace(
+            /\[(.+?)\]/,
+            ` <style type="text/css">
           .word { 
            color: ${color};
           }
          </style><span class="word">${word && word[1]}</span>`
-        );
-        return word ? (
-          <p key={text} dangerouslySetInnerHTML={{ __html: text }} />
-        ) : (
-          <p key={e}>{e}</p>
-        );
-      })}
+          );
+          return word ? (
+            <p key={text} dangerouslySetInnerHTML={{ __html: text }} />
+          ) : (
+            <p key={e}>{e}</p>
+          );
+        })}
+
+        <div>
+          {lang ? "Идея дизайна: " : "Design idea: "}
+          <a href="https://github.com/probberechts/hexo-theme-cactus">
+            hexo-theme-cactus
+          </a>
+        </div>
+      </Content>
     </div>
   );
 };
+
+const Content = styled.div`
+  a {
+    color: ${(props) => props.color};
+  }
+`;
 
 export default About;
